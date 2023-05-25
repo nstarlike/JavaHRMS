bbe13df8772bbed58b6642b51a7c1c107d5da550
nstarlike <46280877+nstarlike@users.noreply.github.com>
2023-05-25 13:09:41 +0900

    Add Spring Boot project

---
+++ src/main/java/nstarlike/hrs/HumanResourceSystemApplication.java
@@ -0,0 +1,13 @@
+package nstarlike.hrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanResourceSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumanResourceSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nstarlike/hrs/HumanResourceSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package nstarlike.hrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumanResourceSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
